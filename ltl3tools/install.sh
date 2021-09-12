5da65eaf4e3d477656d17b71d1779945ad2b99e6
Darius Foo <darius.foo.tw@gmail.com>
2021-09-12 18:55:24 +0800

    Install ltl2mon globally

--- ltl3tools/install.sh
+++ ltl3tools/install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# Installs everything in /, then places an executable in /usr/bin
+
 set -ex
 
 apt-get update && apt-get install curl make build-essential -y
+
+pushd .
+cd /
 
 # AT&T's fsmlibrary
 curl https://www3.cs.stonybrook.edu/~algorith/implement/fsm/distrib/fsm-4_0.linux.x86_64.tar.gz | tar xzv
@@ -14,7 +19,12 @@ curl -L https://github.com/nondeterministic/ltl3tools/releases/download/0.0.8/lt
 curl http://www.lsv.fr/~gastin/ltl2ba/ltl2ba-1.3.tar.gz | tar xzv
 (cd ltl2ba-1.3 && make)
 
-# Install everything
+# ltl3tools
 cd ltl3tools-0.0.8
-ls /fsm-4.0/bin | xargs -L1 -I'{}' ln -snf '/fsm-4.0/bin/{}' $(pwd)/'third-party/{}' && \
+ls /fsm-4.0/bin | xargs -L1 -I'{}' ln -snf "/fsm-4.0/bin/{}" $(pwd)/'third-party/{}' && \
   ln -snf /ltl2ba-1.3/ltl2ba $(pwd)/third-party/ltl2ba
+
+popd
+
+printf '#!/bin/bash\n/ltl3tools-0.0.8/ltl2mon "$1" --prefix /ltl3tools-0.0.8' > /usr/bin/ltl2mon
+chmod +x /usr/bin/ltl2mon
